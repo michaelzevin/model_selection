d8a143a5507143200e0446893fed7901de6f9502
michaelzevin <mzevin1@gmail.com>
2019-05-29 17:55:42 -0500

    changed up selection effects function, should be more flexible and easier to parallelize now

--- populations/utils/selection_effects.py
+++ populations/utils/selection_effects.py
@@ -1,85 +1,56 @@
 #!/usr/bin/env python
 
 """
-Function for incorporating selection effects on a population of binaries.
+Functions for incorporating selection effects on a population of binaries.
 """
 
-import os
 import numpy as np
 import pandas as pd
-import h5py
-import argparse
 
 from scipy.interpolate import interp1d
-from scipy.interpolate import interpn
-from scipy.interpolate import griddata
-
-import itertools
-import multiprocessing
-from functools import partial
-
-import lalsimulation
-import lal
 
 import astropy.units as u
 from astropy import cosmology
 from astropy.cosmology import z_at_value
 
-# import important lal stuff
 import lal, lalsimulation
 from pycbc.detector import Detector
 
 cosmo = cosmology.Planck15
 
-_dirpath = '/projects/b1095/michaelzevin/model_selection/second_generation/data/processed_noredz/'
-_outdir = '/projects/b1095/michaelzevin/model_selection/second_generation/data/processed/'
 
-argp = argparse.ArgumentParser()
-argp.add_argument("-m", "--model", type=str, required=True, help="Name of the model you wish to calculate SNR weights for.")
-argp.add_argument("-n", "--Ntrials", type=int, default=100, help="Define the number of monte carlo trails used for calculating the average SNR. Default=100")
-argp.add_argument("-s", "--snr-min", type=float, default=8, help="Define the SNR threshold for detectability. Default=8")
-argp.add_argument("-mp", "--multiproc", type=int, default=1, help="Number of cores you want to use. Default=1")
-argp.add_argument("-zm", "--z-max", type=str, default=2.0, help="Maximum redshift value for sampling. Default=2.0")
-args = argp.parse_args()
-
-
-# --- Functions --- #
-
-# Spin vector
-def spin_vector(a, phi):
-    """
-    Generates a random spin vector using spin magnitude and tilt.
-    """
-    theta = np.random.uniform(0, 2*np.pi, size=len(a))
-    ax = a*np.sin(theta)*np.cos(phi)
-    ay = a*np.sin(theta)*np.sin(phi)
-    az = a*np.cos(theta)
-    return ax, ay, az
-
-# Comoving volume
-def Vc(z):
-    Dl = cosmo.luminosity_distance(z)
-    return 4./3*np.pi*Dl**3 / (1+z)**3
+# --- Useful functions --- #
 
 # Waveform generator
-def get_waveform(m1, m2, s1, s2, z, vary_params=False, **kwargs):
+def get_waveform(m1, m2, z, s1=None, s2=None, vary_params=False, **kwargs):
+    """
+    Generates waveform. If vary_params=False, will calculate optimal SNR.
+    """
 
-    # read masses
-    m1 = m1*u.Msun.to(u.kg)
-    m2 = m2*u.Msun.to(u.kg)
+    # read (source frame) masses
+    m1 = m1*u.Msun.to(u.kg) * (1+z)
+    m2 = m2*u.Msun.to(u.kg) * (1+z)
 
-    # assume non-spinning
-    s1x, s1y, s1z = s1[0],s1[1],s1[2]
-    s2x, s2y, s2z = s2[0],s2[1],s2[2]
+    # assume non-spinning unless spins provided
+    if s1:
+        s1x, s1y, s1z = s1[0],s1[1],s1[2]
+    else:
+        s1x, s1y, s1z = 0,0,0
+    if s2:
+        s2x, s2y, s2z = s2[0],s2[1],s2[2]
+    else:
+        s2x, s2y, s2z = 0,0,0
 
-    # read f_low, or assume 10 Hz if not in kwargs
-    f_low = kwargs["f_low"] if "f_low" in kwargs else 10. # Stock f_low: 10 Hz
+    # read f_low, df or assume 10 Hz if not in kwargs
+    f_low = kwargs["f_low"] if "f_low" in kwargs else 10.
+    f_high = kwargs["f_high"] if "f_high" in kwargs else 2048.
+    df = kwargs["df"] if "df" in kwargs else 1./32
 
     # read approximant, or assume IMRPhenomPv2
     approx = lalsimulation.GetApproximantFromString(kwargs["approx"] if "approx" in kwargs else "IMRPhenomPv2")
 
     # read distance
-    distance = cosmology.Planck15.comoving_distance(z).value * u.Mpc.to(u.m)
+    distance = cosmology.Planck15.luminosity_distance(z).value * u.Mpc.to(u.m)
 
     if vary_params:
         ra, dec = np.random.uniform(0, 2*np.pi), np.arcsin(np.random.uniform(-1, 1))
@@ -91,7 +62,7 @@ def get_waveform(m1, m2, s1, s2, z, vary_params=False, **kwargs):
     else:
         ra, dec, incl = np.pi, 0.0, 0.0
         psi, phi = 0., 0.
-        fp, fx = 1, 1
+        fp, fx = 1., 0.
         gp, gx = (1 + np.cos(incl)**2)/2, np.cos(incl)
 
 
@@ -112,7 +83,7 @@ def get_waveform(m1, m2, s1, s2, z, vary_params=False, **kwargs):
                                            # meanPerAno
                                            0,
                                            # deltaF, f_min, f_max, f_ref
-                                           df, f_low, 2048., f_low,
+                                           df, f_low, f_high, f_low,
                                            # LALparams
                                            lal.CreateDict(),
                                            # approximant
@@ -121,31 +92,87 @@ def get_waveform(m1, m2, s1, s2, z, vary_params=False, **kwargs):
     hpf.data.data *= fp * gp
     hxf.data.data *= fx * gx
 
-    return hpf, hxf
+    freqs = np.arange(0., f_high+df, df)
+
+    return hpf, hxf, freqs
+
+
+# PSD call function
+def get_psd(psd, ifo, **kwargs):
+    """
+    Gets the PSD from either observing scenarios table or lalsimulation
+    """
+    # read f_low, f_high, and df or assume default values if not in kwargs
+    f_low = kwargs["f_low"] if "f_low" in kwargs else 10.
+    f_high = kwargs["f_high"] if "f_high" in kwargs else 2048.
+
+    psd_path = kwargs["psd_path"] if "psd_path" in kwargs else None
+
+    # try to read tables if strings are provided
+    if type(psd)==str:
+        if ifo in ["H1", "L1"]:
+            psd_data = pd.read_csv(psd_path+'/LIGO_P1200087.dat', sep=' ', index_col=False)
+        elif ifo in ["V1"]:
+            psd_data = pd.read_csv(psd_path+'/Virgo_P1200087.dat', sep=' ', index_col=False)
+        freqs = np.asarray(psd_data["freq"])
+        # observeing scenarios table provides ASDs
+        psd_vals = np.asarray(psd_data[psd])**2
+        psd_interp = interp1d(freqs, psd_vals)
+
+    # otherwise, assume lalsimulation psd was provided
+    else:
+        freqs = np.arange(f_low, f_high+df, df)
+        psd_vals = np.asarray(list(map(psd, freqs)))
+        psd_interp = interp1d(freqs, psd_vals)
+
+    return psd_interp
+
+
+# Detector call function
+def get_detector(ifo):
+    """
+    Gets the detector for the response function
+    """
+    if ifo not in ["H1","L1","V1"]:
+        raise NameError("Detector '{0:s}' not recognized!".format(ifo))
+    det = Detector(ifo)
+    return det
+
 
 # SNR
-def snr(hpf, hxf, psd, freqs, f_low=10):
+def snr(hpf, hxf, freqs, psd, **kwargs):
     """
     Calculates:
     \rho^2 = 4 \int_{f_0}^{f_h} \frac{\tilde{h}^{\conj}(f)\tilde{h}(f)}{S(f)} df
     """
+    # read f_low, df or assume 10 Hz if not in kwargs
+    f_low = kwargs["f_low"] if "f_low" in kwargs else 10.
+    df = kwargs["df"] if "df" in kwargs else 1./32
+
     hf = hpf.data.data + hxf.data.data
     idx = np.where(freqs >= f_low)
     hf_cut = hf[idx]
+    psd_vals = psd(freqs[idx])
 
-    return np.sqrt(4*np.real(np.sum(hf_cut*np.conj(hf_cut)*df/psd)))
+    return np.sqrt(4*np.real(np.sum(hf_cut*np.conj(hf_cut)*df/psd_vals)))
 
-# MC sample over waveforms
-def monte_carlo_waveforms(hpf,hxf):
+
+# Sampling of extrinsic parameters
+def sample_extrinsic(hpf, hxf, det):
+    """
+    Varies extrinsic parameters of waveform for calculating detection probability
+    """
 
     # vary extrinsic parametrers
     ra, dec = np.random.uniform(0, 2*np.pi), np.arcsin(np.random.uniform(-1, 1))
     incl = np.arccos(np.random.uniform(-1, 1))
     psi = np.random.uniform(0, np.pi)
 
+    # inclination
+    gp, gx = (1 + np.cos(incl)**2)/2, np.cos(incl)
+
     # Use pycbc for detector response
     fp, fx = det.antenna_pattern(ra, dec, psi, 1e9)
-    gp, gx = (1 + np.cos(incl)**2)/2, np.cos(incl)
 
     p = fp * gp
     x = fx * gx
@@ -153,113 +180,79 @@ def monte_carlo_waveforms(hpf,hxf):
     return np.sqrt(p**2 + x**2)
 
 
-# Parallelizing function
-def calc_weights(system, calc_redz, snr_min, z_max, Ntrials):
+# Spin vector
+def spin_vector(a, phi):
+    """
+    Generates a random spin vector using spin magnitude and tilt.
+    """
+    theta = np.random.uniform(0, 2*np.pi, size=len(a))
+    ax = a*np.sin(theta)*np.cos(phi)
+    ay = a*np.sin(theta)*np.sin(phi)
+    az = a*np.cos(theta)
+    return ax, ay, az
 
-    # --- sample redshifts flat in comoving volume, if it isn't already provided
-    if calc_redz:
+
+# Comoving volume
+def Vc(z):
+    """
+    Comoving volume from redshift
+    """
+    Dl = cosmo.luminosity_distance(z)
+    return 4./3*np.pi*Dl**3 / (1+z)**3
+
+
+# Detection probability function
+def detection_probability(m1, m2, z=None, s1=None, s2=None, ifos={"H1":"midhighlatelow"}, rho_det=8.0, z_max=2.0, Ntrials=1000, **kwargs):
+    """
+    Calls other functions in this file to calculate a detection probability
+    """
+    # read f_low, df or assume 10 Hz if not in kwargs
+    f_low = kwargs["f_low"] if "f_low" in kwargs else 10.
+    f_high = kwargs["f_high"] if "f_high" in kwargs else 2048.
+    df = kwargs["df"] if "df" in kwargs else 1./32
+    psd_path = kwargs["psd_path"] if "psd_path" in kwargs else None
+
+    # if redshift not provided, calculate it uniform in comoving volume
+    if not z:
         Vc_max = Vc(z_max).value
         randVc = np.random.uniform(0,Vc_max) * u.Mpc**3
         randDc = (3./(4*np.pi)*randVc)**(1./3)
         z = z_at_value(cosmo.comoving_distance, randDc)
 
-    # --- randomly generate waveforms and do SNR calculations
-    m1, m2 = system[0], system[1]
-    s1, s2 = system[2], system[3]
-    if not calc_redz:
-        z = system[4]
+    # get the detectors of choice for the response function
+    detectors = {}
+    for ifo in ifos.keys():
+        detectors[ifo] = get_detector(ifo)
 
-    # calculate optimal SN
-    hpf_opt, hxf_opt = get_waveform(m1, m2, s1, s2, z, vary_params=False)
-    # if optimal SNR < snr_min, just append a weight of 0
-    hxf_opt.data.data *= 0
-    snr_opt = snr(hpf_opt, hxf_opt, psd, freqs, f_low)
-    if snr_opt < float(snr_min):
+    # get the psds
+    psds={}
+    for ifo, psd in ifos.items():
+        psd_interp = get_psd(psd, ifo, f_low=f_low, f_high=f_high, psd_path=psd_path)
+        psds[ifo] = psd_interp
+
+    # calculate optimal SNR
+    hpf_opt, hxf_opt, freqs = get_waveform(m1, m2, z, s1, s2, vary_params=False, f_low=f_low, f_high=f_high, df=df)
+    rho_opts={}
+    for ifo in ifos.keys():
+        rho_opts[ifo] = snr(hpf_opt, hxf_opt, freqs, psds[ifo], f_low=f_low, df=df)
+
+    # if the combined SNR is less than the detection threshold, give weight of 0
+    if np.sqrt(np.sum(np.square(rho_opts.values()))) < float(rho_det):
         weight = 0.0
+
     else:
         snrs = []
         for i in range(Ntrials):
-            snr_factor = monte_carlo_waveforms(hpf_opt,hxf_opt)
-            snrs.append(snr_opt*snr_factor)
+            network_snr = []
+            for ifo, det in detectors.items():
+                rho_factor = sample_extrinsic(hpf_opt, hxf_opt, det)
+                network_snr.append(rho_opts[ifo]*rho_factor)
+            snrs.append(np.sqrt(np.sum(np.square(network_snr))))
         # now, we see what percentage of SNRs passed our threshold
-        passed = sum(1 for i in snrs if i>=float(snr_min))
+        passed = sum(1 for i in snrs if i>=float(rho_det))
         weight = float(passed) / len(snrs)
 
-    return z, weight
+    return weight
 
-
-def normalize_weights(x, low=0):
-    # normalizes between 0 and 1
-    if all(t==0 for t in x):
-        return np.zeros_like(x)
-    else:
-        return((x-low)/(x.max()-low))
-
-
-### MAIN FUNCTION ###
-
-# --- read in the population
-channels = list(h5py.File(_dirpath+args.model+'.hdf', 'r').keys())
-
-for channel in channels:
-    print(channel)
-
-    pop = pd.read_hdf(_dirpath+args.model+'.hdf', key=channel)
-
-    # --- generate spin vectors with randomized azimuthal angle
-    pop['s1x'], pop['s1y'], pop['s1z'] = spin_vector(pop['a1'], np.arccos(pop['cos_t1']))
-    pop['s2x'], pop['s2y'], pop['s2z'] = spin_vector(pop['a2'], np.arccos(pop['cos_t2']))
-
-    # --- get the PSDs
-    df = 1./32
-    f_low = 10.
-    freqs = np.arange(f_low, 2048 + df, df)
-    psd = np.asarray(list(map(lalsimulation.SimNoisePSDaLIGOZeroDetHighPower, freqs)))
-
-    # --- get the response function for IFO of choice
-    det = Detector("H1")
-
-    # --- set up partial functions and organize data for multiprocessing
-    systems_info = []
-    if 'z' in pop.keys():
-        func = partial(calc_weights, calc_redz=False, snr_min=args.snr_min, z_max=args.z_max, Ntrials=args.Ntrials)
-        for idx, system in pop.iterrows():
-            systems_info.append([system['m1'], system['m2'], (system['s1x'],system['s1y'],system['s1z']), (system['s2x'],system['s2y'],system['s2z']), system['z']])
-    else:
-        func = partial(calc_weights, calc_redz=True, snr_min=args.snr_min, z_max=args.z_max, Ntrials=args.Ntrials)
-        for idx, system in pop.iterrows():
-            systems_info.append([system['m1'], system['m2'], (system['s1x'],system['s1y'],system['s1z']), (system['s2x'],system['s2y'],system['s2z'])])
-
-    if args.multiproc > 1:
-        mp = int(args.multiproc)
-        pool = multiprocessing.Pool(mp)
-        results = pool.map(func, systems_info)
-        pool.close()
-        pool.join()
-    else:
-        results = []
-        for system in systems_info:
-            results.append(func(system))
-
-    results = np.transpose(results)
-    z = results[0]
-    weight = results[1]
-
-    # record redshifts
-    if 'z' not in pop.keys():
-        pop['z'] = z
-
-    # record weights
-    if 'clus_weight' in pop.keys():
-        combined_weight = pop['clus_weight'] * weight
-        pop['weight'] = normalize_weights(combined_weight)
-    else:
-        pop['weight'] = normalize_weights(weight)
-
-    # get rid of systems with 0 weight
-    pop = pop.loc[pop['weight'] > 0]
-
-    # save pop model
-    pop.to_hdf(_outdir+args.model+'.hdf', key=channel)
 
 
