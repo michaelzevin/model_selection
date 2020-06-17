c7990f7f5932c5502bb42f61f77672cf3265f33a
michaelzevin <mzevin1@gmail.com>
2020-06-16 19:07:53 -0500

    new and improved bounded_Nd_kde

--- populations/utils/bounded_Nd_kde.py
+++ populations/utils/bounded_Nd_kde.py
@@ -11,11 +11,11 @@ from scipy.stats import gaussian_kde as kde
 import itertools
 
 class Bounded_Nd_kde(kde):
-    r"""Represents an N-dimensional Gaussian kernel density estimator
+    """Represents an N-dimensional Gaussian kernel density estimator
     for a probability distribution function that exists on a bounded
     domain."""
 
-    def __init__(self, pts, bounds=None, *args, **kwargs):
+    def __init__(self, pts, bounds=None, weights=None, bw_method='scott', **kwargs):
         """Initialize with the given bounds.  ``bounds`` should have
         same dimensions as pts, or can be supplied as ``None``.  Extra
         parameters are passed to :class:`gaussian_kde`.
@@ -36,10 +36,62 @@ class Bounded_Nd_kde(kde):
             bounds = np.atleast_2d(bounds)
             assert np.asarray(bounds).shape == (pts.shape[0],2), 'If bounds are supplied, must have same dimensions as pts'    
 
-        super(Bounded_Nd_kde, self).__init__(pts, *args, **kwargs)
+        Ndim = pts.shape[0]
 
-        self._Ndim = pts.shape[0]
+        ### Reflect points over specified bounds ###
+        refl_norm_factor = 1
+
+        # first, reflect over the 2n sides of the hypercube
+        if bounds is not None:
+            pts0 = pts.copy()
+            if weights is not None:
+                weights0 = weights.copy()
+            for dim in np.arange(Ndim):
+                for idx, edge in enumerate(bounds[dim]):
+                    if edge is not None:
+                        refl_pts = pts0.copy() #Ndim x Npts
+                        refl_pts[dim] = 2*edge - refl_pts[dim] 
+                        pts = np.concatenate([pts, refl_pts], axis=1)
+                        if weights is not None:
+                            weights = np.concatenate([weights, weights0])
+                        # keep track of how many reflections we've made
+                        refl_norm_factor += 1
+
+            # next, reflect over the 2^n vertices of the hypercube
+            if Ndim > 1:
+                # keep track of lower (0) and upper (1) bounds
+                which_bound = np.asarray([[0,1]]*len(bounds))
+                # permutate over all the vertices
+                for ul, vertex in zip(list(itertools.product(*which_bound)), \
+                                    list(itertools.product(*bounds))):
+                    # if any dimension at that vertex is unbounded, continue
+                    if any(np.asarray(vertex) == None):
+                        continue
+                    # determine which points at that vertex are lower and upper bounds
+                    lower_bounds = (np.asarray(ul) == 0)
+                    upper_bounds = (np.asarray(ul) == 1)
+                    assert all(lower_bounds == ~upper_bounds), 'cannot have the same lower and upper bound'
+                    # get reflected points at this vertex
+                    refl_pts = pts0.copy() #Ndim x Npts
+                    for dim in np.arange(Ndim):
+                        if lower_bounds[dim]:
+                            refl_pts[dim] = 2*bounds[dim,0] - refl_pts[dim]
+                        else:
+                            refl_pts[dim] = 2*bounds[dim,1] - refl_pts[dim]
+
+                    pts = np.concatenate([pts, refl_pts], axis=1)
+                    if weights is not None:
+                        weights = np.concatenate([weights, weights0])
+                    # keep track of how many reflections we've made
+                    refl_norm_factor += 1
+
+        # inheret stuff from Scipy's kde class
+        super(Bounded_Nd_kde, self).__init__(pts, bw_method=bw_method, weights=weights)
+
+        # add reflection normalization factor as attribute
+        self._Ndim = Ndim
         self._bounds = bounds
+        self._refl_norm_factor = refl_norm_factor
 
     @property
     def Ndim(self):
@@ -51,65 +103,17 @@ class Bounded_Nd_kde(kde):
         """The lower and upper bounds of the N domains."""
         return self._bounds
 
-    def evaluate(self, pts):
+    def evaluate(self, eval_pts):
         """Return an estimate of the density evaluated at the given points."""
-        pts = np.atleast_2d(pts)
-        assert pts.ndim == 2, 'points must be two-dimensional'
-        assert pts.shape[0] == self.Ndim, 'evaluation points must be same number of dimensions as kde'
+        eval_pts = np.atleast_2d(eval_pts)
+        assert eval_pts.ndim == 2, 'points must be two-dimensional'
+        assert eval_pts.shape[0] == self.Ndim, 'evaluation points must be same number of dimensions as kde'
 
-        # inherit original instance of kde
-        pdf = super(Bounded_Nd_kde, self).evaluate(pts)
+        # inherit original instance of kde and evaluate points
+        pdf = super(Bounded_Nd_kde, self).evaluate(eval_pts)
 
-        # keep track of extra volume analyzed
-        original_vol = np.sum(pdf)
-        extra_vol = 0
-
-        ### first, reflect over the 2n sides of the hypercube
-        if self.bounds is not None:
-            for dim in np.arange(self.Ndim):
-                for idx, edge in enumerate(self.bounds[dim]):
-                    if edge is not None:
-                        refl_pts = pts.copy() #Ndim x Npts
-                        refl_pts[dim] = 2*edge - refl_pts[dim] 
-                        new_pdf = super(Bounded_Nd_kde, self).evaluate(refl_pts)
-                        pdf += new_pdf
-                        extra_vol += np.sum(new_pdf)
-                
-        ### next, reflect over the 2^n vertices of the hypercube
-        if self.bounds is not None:
-            # keep track of lower (0) and upper (1) bounds
-            which_bound = np.asarray([[0,1]]*len(self.bounds))
-            # permutate over all the vertices
-            for ul, vertex in zip(list(itertools.product(*which_bound)), \
-                                list(itertools.product(*self.bounds))):
-                # if any dimension at that vertex is unbounded, continue
-                if any(np.asarray(vertex) == None):
-                    continue
-                # determine which points at that vertex are lower and upper bounds
-                lower_bounds = (np.asarray(ul) == 0)
-                upper_bounds = (np.asarray(ul) == 1)
-                assert all(lower_bounds == ~upper_bounds), 'cannot have the same lower and upper bound'
-                # get reflected points at this vertex
-                refl_pts = pts.copy() #Ndim x Npts
-                for dim in np.arange(self.Ndim):
-                    if lower_bounds[dim]:
-                        refl_pts[dim] = 2*self.bounds[dim,0] - refl_pts[dim]
-                    else:
-                        refl_pts[dim] = 2*self.bounds[dim,1] - refl_pts[dim]
-
-                new_pdf = super(Bounded_Nd_kde, self).evaluate(refl_pts)
-                pdf += new_pdf
-                extra_vol += np.sum(new_pdf)
-
-        # get the final normalization factor
-        # FIXME this doesn't seem to be working, come back to this
-        # FIXME also...tightly bounded KDE with reflect values 
-        # over the other bound...
-        if (self.bounds is not None) and any(self.bounds.flatten()!=None):
-            norm_fac = 1
-            #norm_fac = extra_vol / (original_vol + extra_vol)
-        else:
-            norm_fac = 1
+        # return extra normalization factor for the reflections
+        norm_fac = self._refl_norm_factor
 
         return pdf, norm_fac
 
@@ -147,6 +151,8 @@ class Bounded_Nd_kde(kde):
                     out_of_bounds[pts[dim, :] > self.bounds[dim,1]] = True
 
         results, norm_fac = self.evaluate(pts)
+
         # set out-of-bound evaluations to 0
-        results[out_of_bounds] = 0.
+        results[out_of_bounds] = 0.0
+
         return norm_fac*results
